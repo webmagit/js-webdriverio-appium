05c1a4ad26c88a4c6a3065bbb5155faef2709ed3
Kanak Kalburgi <kanak@webmagic.in>
2020-04-14 09:44:50 +1000

    Format code using prettier
    * ‘npm run validate’ will lint and format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    parserOptions: {
-        ecmaVersion: 2018,
-    },
-    rules: {
-        'prefer-destructuring': 'warn',
-        'class-methods-use-this': 'off',
-        'no-unused-expressions': 'off',
-        'global-require': 'off',
-        'func-names': 'off',
-        'no-param-reassign': 'off',
-        'import/prefer-default-export': 'off',
-        'no-magic-numbers': 'off',
-        'no-useless-escape': 'off',
-        'no-underscore-dangle': 'off',
-    },
-    globals: {
-        browser: true,
-        $: true,
-        $$: true,
-        driver: true,
-        expect: true,
-        document: true,
-    },
-    env: {
-        mocha: true,
-        amd: true,
-    },
-    extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "prefer-destructuring": "warn",
+    "class-methods-use-this": "off",
+    "no-unused-expressions": "off",
+    "global-require": "off",
+    "func-names": "off",
+    "no-param-reassign": "off",
+    "import/prefer-default-export": "off",
+    "no-magic-numbers": "off",
+    "no-useless-escape": "off",
+    "no-underscore-dangle": "off",
+  },
+  globals: {
+    browser: true,
+    $: true,
+    $$: true,
+    driver: true,
+    expect: true,
+    document: true,
+  },
+  env: {
+    mocha: true,
+    amd: true,
+  },
+  extends: ["eslint:recommended"],
 };
--- test/specs/basic.test.js
+++ test/specs/basic.test.js
@@ -1,9 +1,12 @@
-const assert = require('assert')
+const assert = require("assert");
 
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io')
-        const title = browser.getTitle()
-        assert.strictEqual(title, 'WebdriverIO · Next-gen browser automation test framework for Node.js')
-    })
-})
+describe("webdriver.io page", () => {
+  it("should have the right title", () => {
+    browser.url("https://webdriver.io");
+    const title = browser.getTitle();
+    assert.strictEqual(
+      title,
+      "WebdriverIO · Next-gen browser automation test framework for Node.js"
+    );
+  });
+});
