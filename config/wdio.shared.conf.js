66fc2e00b8c291379667a9d3c18adfa42c790fb2
Kanak Kalburgi <kanak@webmagic.in>
2020-04-16 09:19:09 +1000

    feat: update browserstack with test status

--- config/wdio.shared.conf.js
+++ config/wdio.shared.conf.js
@@ -1,6 +1,10 @@
+let FAILURE_REASON = ''
+let browserstack_username = process.env.BROWSERSTACK_USER
+let browserstack_password = process.env.BROWSERSTACK_ACCESSKEY
+
 exports.config = {
-  user: process.env.BROWSERSTACK_USER,
-  key: process.env.BROWSERSTACK_ACCESSKEY,
+  user: browserstack_username,
+  key: browserstack_password,
 
   updateJob: false,
   exclude: [],
@@ -18,4 +22,45 @@ exports.config = {
     defaultTimeoutInterval: 90000,
   },
   reporters: ['spec'],
+
+  afterTest: function (test, context, { error }) {
+    if (error) {
+      // stack is available at error.stack
+      FAILURE_REASON = error.message
+    }
+  },
+  after: function (result) {
+    let request = require('request')
+
+    let browserstack_sessionID = driver.sessionId
+    let testStatus,
+      reason = ''
+
+    if (result == 1) {
+      testStatus = 'failed'
+      reason = FAILURE_REASON || ''
+    } else if (result == 0) {
+      testStatus = 'passed'
+    }
+
+    request(
+      {
+        uri:
+          'https://' +
+          browserstack_username +
+          ':' +
+          browserstack_password +
+          '@api-cloud.browserstack.com/app-automate/sessions/' +
+          browserstack_sessionID +
+          '.json',
+        method: 'PUT',
+        form: { status: testStatus, reason: reason },
+      },
+      function (error) {
+        if (error) {
+          console.log('Failed to mark Browserstack Status:', error)
+        }
+      },
+    )
+  },
 }
